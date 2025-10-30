48a202a0f55b0414a83957526ea5f5f9e346c552
Tai <nhantaivang9999@gmail.com>
2025-10-30 08:59:03 +0700

    Backend: Thêm Node.js server với CRUD API cho User

---
+++ backend/controllers/userController.js
@@ -0,0 +1,30 @@
+let users = [];
+let nextId = 1;
+
+exports.getUsers = (req, res) => {
+  res.json(users);
+};
+
+exports.createUser = (req, res) => {
+  const { name, email } = req.body;
+  const newUser = { id: nextId++, name, email };
+  users.push(newUser);
+  res.status(201).json(newUser);
+};
+
+exports.updateUser = (req, res) => {
+  const { id } = req.params;
+  const index = users.findIndex(u => u.id == id);
+  if (index !== -1) {
+    users[index] = { ...users[index], ...req.body };
+    res.json(users[index]);
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
+};
+
+exports.deleteUser = (req, res) => {
+  const { id } = req.params;
+  users = users.filter(u => u.id != id);
+  res.json({ message: "User deleted" });
+};
