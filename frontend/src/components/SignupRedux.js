a95cc230b144d6b1541aa84c570a5d9151ed2638
Tai <nhantaivang9999@gmail.com>
2025-11-09 00:05:33 +0700

    Fix ESLint warning: remove unused useDispatch import

--- frontend/src/components/SignupRedux.js
+++ frontend/src/components/SignupRedux.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 
